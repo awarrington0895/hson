20298a7af46998f3555b312fcaf88a709444667d
Alex <awarrington0895@gmail.com>
2023-04-28 23:55:27 +0000

    jsonNull & jsonBool

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
+import Control.Applicative
 import Data.Char
-import Text.ParserCombinators.ReadPrec (reset)
+
+-- import Text.ParserCombinators.ReadPrec (reset)
 
 data JsonValue
   = JsonNull
+  | JsonBool Bool
   | JsonNumber Integer
   | JsonString String
   | JsonArray [JsonValue]
@@ -28,22 +31,34 @@ instance Applicative Parser where
     (rest', a) <- p2 rest
     Just (rest', f a)
 
+instance Alternative Parser where
+  empty = Parser $ const Nothing
+  (Parser p1) <|> (Parser p2) = Parser $ \input -> p1 input <|> p2 input
+
 jsonNull :: Parser JsonValue
-jsonNull = undefined
+jsonNull = JsonNull <$ stringP "null"
+
+jsonBool :: Parser JsonValue
+jsonBool = toBool <$> (stringP "true" <|> stringP "false")
+  where toBool "true" = JsonBool True
+        toBool "false" = JsonBool False
+
+jsonNumber :: Parser JsonValue
+jsonNumber = undefined
+
+jsonValue :: Parser JsonValue
+jsonValue = jsonNull <|> jsonBool
 
 charP :: Char -> Parser Char
 charP x = Parser f
   where
-    f (y:ys)
+    f (y : ys)
       | y == x = Just (ys, x)
       | otherwise = Nothing
     f [] = Nothing
 
 stringP :: String -> Parser String
 stringP = traverse charP
-
-jsonValue :: Parser JsonValue
-jsonValue = undefined
 
 main :: IO ()
 main = do
